e436c917e4b9b4e5647c363dbf2ab75399bb1556
Wayne Graham <wayne.graham@virginia.edu>
2014-06-16 19:44:22 -0400

    Updates for categories

---
+++ _plugins/tag_author.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  class AuthorTag < Liquid::Tag
+
+    def initialize(tag_name, attribute, tokens)
+      super
+      @attribute = attribute
+    end
+
+    def render(context)
+      authors = JSON.parse(IO.read('authors.json'))['authors']
+      author_name = context.environments.first['page']['author']
+      selected_author = authors.select { |author| author['name'] == author_name }
+      selected_author[0][@attribute.strip!]
+    end
+  end
+end
+
+Liquid::Template.register_tag('render_author', Jekyll::AuthorTag)
---
+++ _posts/2013-06-13-wayne-is-awesome.md
@@ -0,0 +1,9 @@
+---
+title: test
+layout: post
+date: 2014-06-16 15:27:22
+category: institutions undergraduate graduate
+---
+# text
+
+Some text
